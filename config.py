2d4a82e3aaa4b161513515c23b8b902c99adff7b
KaveriChowdary <135950891+KaveriChowdary@users.noreply.github.com>
2025-09-01 21:52:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'cmsserver12.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cmsadmin'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'cmsadmin'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'CMSpassword12'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
