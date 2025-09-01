1778ceb32d0200a48535c5e40251888b5f2226a1
KaveriChowdary <135950891+KaveriChowdary@users.noreply.github.com>
2025-09-01 11:58:07 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,7 +2,7 @@
 This script runs the FlaskWebProject application using a development server.
 """
 
-from os import environ
+'''from os import environ
 from FlaskWebProject import app
 
 if __name__ == '__main__':
@@ -11,4 +11,16 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT, ssl_context='adhoc')
+    app.run(HOST, PORT, ssl_context='adhoc')'''
+"""
+This script runs the FlaskWebProject application using a development server.
+"""
+
+from os import environ
+from FlaskWebProject import app
+
+if __name__ == '__main__':
+    # Azure sets the PORT environment variable dynamically
+    port = int(environ.get("PORT", 80))  # default to 80 if PORT not set
+    app.run(host="0.0.0.0", port=port)   # listen on all network interfaces
+
