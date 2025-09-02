c589e30bd9b04fb088dcdc4b7aecff56036a8714
KaveriChowdary <135950891+KaveriChowdary@users.noreply.github.com>
2025-09-02 12:13:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 This script runs the FlaskWebProject application using a development server.
 """
 
+from os import environ
 from FlaskWebProject import app
-import os
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)  # Change 5000 to your preferred port
-
-
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, ssl_context='adhoc')
 """
 This script runs the FlaskWebProject application using a development server.
 """
